8623a4536dea0def9e3449acf89c29aa40adbe2c
raulzitoe <raulguil@gmail.com>
2020-06-27 09:54:52 -0400

    server sends game messages to client

--- server.py
+++ server.py
@@ -4,58 +4,105 @@ import pickle
 import time
 import random
 import constants as c
-from player import Player
+#from player import Player
 from sprites import Sprites
 from collections import defaultdict
 
 def spawn_random():
+    print("Respawn")
     x = random.randint(0, c.MAP_WIDTH-1)
     y = random.randint(0, c.MAP_HEIGHT-1)
     while c.game_map[y][x] != 0:
         x = random.randint(0, c.MAP_WIDTH-1)
         y = random.randint(0, c.MAP_HEIGHT-1)
-    return x + 0.5, y + 0.5
+    return x, y
+
+def calculate_distance(a, b):
+        return ((a.x - b.x)**2 + (a.y - b.y)**2)**(0.5)
+
+def check_death(my_id, dict_sprites):
+    my_sprite = None
+    for player_id, sprites_list in dict_sprites.items():
+        if int(player_id) == my_id:
+            for sprite in sprites_list:
+                if sprite.is_player:
+                    my_sprite = sprite
+    
+    for player_id, sprites_list in dict_sprites.items():
+        if int(player_id) != my_id:
+            for sprite in sprites_list:
+                if not sprite.is_player:
+                    if calculate_distance(my_sprite, sprite) < 0.4:
+                        #print("Death distance: {}".format(calculate_distance(my_sprite, sprite)))
+                        return player_id
+    return False
 
 def threaded_client(conn, _id):
-    global players, sprites_dict, connections
+    global sprites_dict, connections, message
 
     current_id = _id
 
-    # Recieve player name from client
+    # Receive player name from client
     data = conn.recv(16)
     name = data.decode("utf-8")
     print("[SERVER]", name, "connected to the server.")
 
     # Send id to client and create a new player instance with that id
     conn.send(str.encode(str(current_id)))
-    players[current_id] = Player()
+    sprites_dict[current_id].append(Sprites(1, 1, 1, 1, None, 0, True, 1, 1, name))
+    message[current_id] = ""
+    scoreboard[current_id] = [0, 0]
+    for key in message.keys():
+        message[key] = "{} connected to the server".format(name).upper()
 
     # Main loop for in/outbound data handling
     while True:
-        for key, sprites in sprites_dict.items():
+        for player_id, sprites in sprites_dict.items():
             for sprite in sprites:
                 sprite.move()
-                if c.game_map[int(sprite.x)][int(sprite.y)]:
-                    sprites_dict[key].remove(sprite)
+                if c.game_map[int(sprite.x)][int(sprite.y)] and not sprite.is_player:
+                    sprites_dict[player_id].remove(sprite)
+
+        # If player got killed, change death info to new spawn point
+        death_info = [0, 0]
+        killer = check_death(current_id, sprites_dict)
+        if killer:
+            death_info = spawn_random()
+            for player_id, sprites in sprites_dict.items():
+                for sprite in sprites:
+                    if sprite.is_player and player_id == killer:
+                        name1 = sprite.name
+                        scoreboard[int(player_id)][0] += 1
+                    if sprite.is_player and int(player_id) == current_id:
+                        name2 = sprite.name
+                        scoreboard[int(player_id)][1] += 1
+            for key in message.keys():
+                message[key] = "{} killed {}".format(name1, name2).upper()
 
         try:    
             # Keep sending position data to client
-            send_data = pickle.dumps((players, sprites_dict))
+            send_data = pickle.dumps((sprites_dict, death_info, message[current_id], scoreboard))
+            message[current_id] = ""
             conn.send(send_data)
+            
         except Exception as e:
             print(e)
-
+        
         try:
             # Receive data from client
-            data = conn.recv(1024)
+            data = conn.recv(2048)
             if not data:
                 break
             x, y, projectile_data = pickle.loads(data)
 
             # Update player position
             if data:
-                players[current_id].x = x
-                players[current_id].y = y
+                for player_id, sprites in sprites_dict.items():
+                    if int(player_id) == current_id:
+                        for sprite in sprites:
+                            if sprite.is_player:
+                                sprite.x = x
+                                sprite.y = y
                 if projectile_data:
                     sprites_dict[current_id].append(projectile_data)
         except Exception as e:
@@ -64,8 +111,10 @@ def threaded_client(conn, _id):
 
     # When user disconnects	
     print("[SERVER] Name:", name, ", Client Id:", current_id, "disconnected")
+    for key in message.keys():
+        message[key] = "{} disconnected".format(name).upper()
     connections -= 1 
-    del players[current_id]  # remove client information from players list
+    del sprites_dict[current_id]  # remove client information from players list
     conn.close()  # close connection
 
 
@@ -93,11 +142,13 @@ S.listen()  # Listen for connections
 print("[SERVER] Server Started with local ip {} on port {}".format(SERVER_IP, PORT))
 
 # Dynamic variables
-players = {}
+#players = {}
 sprites_dict = defaultdict(list)
 connections = 0
 _id = 0
 threads = []
+message = {}
+scoreboard = {}
 
 print("[SERVER] Waiting for connections")
 
